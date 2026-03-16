a9890dfa53b597e4fe121c6ccab059a4c269fa55
katakamrupendra <katakamrupendra@gmail.com>
2026-03-16 12:45:48 +0530

    feat: Introduce Fahrenheit to Celsius conversion, add 'Hello World' output, and log intermediate fibonacci values.

--- rust_1.rs
+++ rust_1.rs
@@ -1,10 +1,14 @@
 fn main() {
-    let fib = fibonacci(10);
-    println!("The 10th fibonacci number is: {}", fib);
+    println!("Hello World");
     let fibi = fibo(10);
     println!("The 10th fibonacci number is: {}", fibi);
+    let t = 100.0;
+    println!("The temperature in celsius is: {}", temp(t));
 }
 
+fn temp(t:f64) -> f64{
+    (t-32.0)*5.0/9.0
+}
 // fibonaci generator  -- iteration 1
 fn fibonacci(n:i32) -> i32 {
     if n <= 1{
@@ -24,6 +28,8 @@ fn fibo(n:usize) -> usize{ //what is usize
         let temp = a;
         a= b;
         b = temp + a;
+        println!("{}",a);
     }
     a
+
 }
