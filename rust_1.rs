a147e84f287327bdee6c682fb47739b2a6e1db77
katakamrupendra <katakamrupendra@gmail.com>
2026-03-16 12:40:35 +0530

    feat: Add an iterative Fibonacci function and integrate it into the main function.

--- rust_1.rs
+++ rust_1.rs
@@ -1,9 +1,11 @@
 fn main() {
     let fib = fibonacci(10);
     println!("The 10th fibonacci number is: {}", fib);
+    let fibi = fibo(10);
+    println!("The 10th fibonacci number is: {}", fibi);
 }
 
-// fibonaci generator 
+// fibonaci generator  -- iteration 1
 fn fibonacci(n:i32) -> i32 {
     if n <= 1{
         n
@@ -12,4 +14,16 @@ fn fibonacci(n:i32) -> i32 {
         fibonacci(n-1) + fibonacci(n-2)
     }
 }
-    
+
+// fibonaci generator  -- iteration 2
+fn fibo(n:usize) -> usize{ //what is usize
+    let mut a =0;
+    let mut b=1;
+
+    for _ in 0..n{
+        let temp = a;
+        a= b;
+        b = temp + a;
+    }
+    a
+}
